e00468c6a41106762f3f11fd889dd55bb57381e4
himanshu-tyd <himanshutaviyad0072@gmail.com>
2024-11-04 19:12:23 +0530

    assinment-done

--- WEB_DEV/WEEK_2_ASYNC_JS/assignment-1.js
+++ WEB_DEV/WEEK_2_ASYNC_JS/assignment-1.js
@@ -1,3 +1,17 @@
 //Try to create a promisified version of setTimeOut
 // fetch
 //fs.FileRead
+
+const promise=new Promise((resolve, reject)=>{
+    setTimeout(() => {
+           if(resolve){
+            return console.log('foo')
+           }
+
+           if(reject){
+            return console.log('reject')
+           }
+    }, (1000));
+})
+
+promise.then((data)=>console.log(data))
