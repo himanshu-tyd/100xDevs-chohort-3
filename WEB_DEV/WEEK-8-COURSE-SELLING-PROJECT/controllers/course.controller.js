4a57cbe90be8bd633ce25ccc61fe6ac4048262fe
himanshu-tyd <himanshutaviyad0072@gmail.com>
2024-12-21 23:47:23 +0530

    feat: admin sigin , signup , createCourse added end-point and middlware for authetication

--- WEB_DEV/WEEK-8-COURSE-SELLING-PROJECT/controllers/course.controller.js
+++ WEB_DEV/WEEK-8-COURSE-SELLING-PROJECT/controllers/course.controller.js
@@ -1,17 +1,95 @@
+import { AdminModel, CourseModel } from "../models/models.js";
+import { compareHash, generateHash } from "../utils/helper.js";
+import { GenerateCookie } from "../utils/verify.js";
 
-export const signup=(req,res)=>{
-    res.send('adming signup route')
-}
+export const signup = async (req, res) => {
+  const { email, password, fullname } = req.body;
 
-export const signin=(req,res)=>{
-    res.send('admin signin route')
-}
+  if (!email || !password || !fullname) {
+    return res.json({
+      success: false,
+      message: "please provide valid details.",
+    });
+  }
 
-export const createCourse=(req,res)=>{
-    res.send('create course route')
-}
+  const user = await AdminModel.findOne({ email });
 
-export const purchase=(req,res)=>{
-    res.send("purchase course route")
-}
+  if (user) {
+    return res.json({ success: false, message: "Admin Created Successfully " });
+  }
 
+  const newAdmin = await AdminModel.create({
+    email,
+    fullname,
+    password: await generateHash(password),
+  });
+
+  if (!newAdmin) {
+    res.json({ success: false, message: "faile to register data" });
+  }
+
+  res.json({
+    success: true,
+    message: "Admin register successfully",
+    data: newAdmin,
+  });
+};
+
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.json({ success: false, message: "All field are required" });
+  }
+
+  const admin = await AdminModel.findOne({ email });
+
+  if (!admin) {
+    return res.json({ success: false, message: "Invalid creadintials." });
+  }
+
+  //check hash password
+
+  const isValidPass = await compareHash(password, admin);
+
+  if (!isValidPass) {
+    return res.json({ success: false, message: "Invalid creadintials." });
+  }
+
+  // generate cookie here and send this to client
+
+  GenerateCookie(admin._id, res);
+
+  res.json({ success: true, message: "Admin Login successfully", data: admin });
+};
+
+export const createCourse = async (req, res) => {
+  const userId = req.userId;
+  const { title, description, price, imageUrl } = req.body;
+
+  if (!title || !description || !price) {
+    return res.json({ success: false, message: "all fieds are requre" });
+  }
+
+  const newCourse = await CourseModel.create({
+    title,
+    description,
+    price,
+    imageUrl,
+    creatorId: userId,
+  });
+
+  if (!newCourse) {
+    return res.json({ success: false, message: "failed to create course" });
+  }
+
+  res.json({
+    success: true,
+    message: "Coure register successfully",
+    data: newCourse,
+  });
+};
+
+export const purchase = (req, res) => {
+  res.send("purchase course route");
+};
--- WEB_DEV/WEEK-8-COURSE-SELLING-PROJECT/routes/course.route.js
+++ WEB_DEV/WEEK-8-COURSE-SELLING-PROJECT/routes/course.route.js
@@ -7,12 +7,13 @@ import {
   signin,
   purchase,
 } from "../controllers/course.controller.js";
+import { isAuthenticate } from "../middlwares/authentication.js";
 
 const route = express.Router();
 
-route.post("/sigup",signup);
+route.post("/signup",signup);
 route.post("/signin",signin);
-route.post('/create', createCourse)
+route.post('/create', isAuthenticate ,createCourse)
 route.post('/purchase', purchase)
 
 export default route
