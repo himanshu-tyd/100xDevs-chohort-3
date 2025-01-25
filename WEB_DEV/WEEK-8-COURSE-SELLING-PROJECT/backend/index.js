fca23d8350b0ec330d1ca17dc191dc7bedd71271
himanshu-tyd <himanshutaviyad0072@gmail.com>
2025-01-25 14:06:24 +0530

    feat: hostig configration

--- WEB_DEV/WEEK-8-COURSE-SELLING-PROJECT/backend/index.js
+++ WEB_DEV/WEEK-8-COURSE-SELLING-PROJECT/backend/index.js
@@ -5,12 +5,15 @@ import connectDB from "./database/connection.js"
 import userRoute from "./routes/user.route.js"
 import courseRoute from './routes/course.route.js'
 import adminRoute from './routes/admin.route.js'
+import path from 'path'
 
 dotevn.config()
 
 
 const port=8000
 const app=express()
+const __dirname=path.resolve()
+console.log(__dirname)
 
 app.use(express.json())
 app.use(cookieParser())
@@ -19,10 +22,15 @@ app.use('/api/users',userRoute)
 app.use('/api/admin', adminRoute) 
 app.use('/api/course' , courseRoute )
 
+app.use(express.static(path.join(__dirname , "/frontend/dist")))
 
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
 
 app.listen(port ,()=>{
-    connectDB()
     console.log(`server is running at port ${port}`)
+    connectDB()
     
 })
